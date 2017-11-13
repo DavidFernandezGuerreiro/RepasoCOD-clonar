f33f2365585d557e6edb6dcd3c8eece5413b6163
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-11-13 11:30:42 +0100

    INICIO PROGRAMA. Programa para ser clonado.

---
+++ src/repasocod/clonar/RepasoCODClonar.java
@@ -0,0 +1,19 @@
+
+package repasocod.clonar;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class RepasoCODClonar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Proyecto que voy a subir a Github para clonar.
+        System.out.println("Hola mundo");
+        System.out.println("Lo subo a git");
+    }
+    
+}
